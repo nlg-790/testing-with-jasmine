5d1eb6e7d47f1b1841bb93a06bad61700b45e6d3
Nick Garcia <nick@Nicks-MacBook-Pro.local>
2022-06-07 10:44:52 -0400

    initial commit

---
+++ taxes.js
@@ -0,0 +1,25 @@
+let income=true;
+
+function calculateTaxes(income) {
+    if(!Number.isFinite(income)){
+        throw new Error('Invalid Income!')
+    }
+    if (income > 30000) {
+        return income * 0.25;
+    }
+    else {
+        return income * 0.15;
+    }
+}
+
+function removeDupes(values){
+    const arr = [...new Set(values)];
+    if(typeof values === 'string') return arr.join('')
+    return arr;
+}
+
+function remove(arr, val){
+    return arr.filter((el) => {
+        return el !== val
+    })
+}
